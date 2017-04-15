f5a3400c54c8079bf1c6ce90e008704b5fa94bc8
Amit Saxena <Ravinder.Kundarapu@ncr.com>
2017-04-15 06:37:52 +0530

    Error removed

--- NotificationType/app/src/main/java/com/programmingtalents/android/notification/NextClass.java
+++ NotificationType/app/src/main/java/com/programmingtalents/android/notification/NextClass.java
@@ -1,14 +1,9 @@
 package com.programmingtalents.android.notification;
 
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
-import com.acadgild.android.notificationtype.R;
 
 /**
  * Created by me on 7/15/2016.
